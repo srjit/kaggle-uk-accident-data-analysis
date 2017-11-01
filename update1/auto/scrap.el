88bf1d7c1b7c5f2115b06e92431423298d6f4aac
Sreejith S <sreejith2904@gmail.com>
2017-11-01 00:55:11 -0400

    add more eda

---
+++ update1/auto/scrap.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook
+ "scrap"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (LaTeX-add-labels
+    "fig:example"))
+ :latex)
+
